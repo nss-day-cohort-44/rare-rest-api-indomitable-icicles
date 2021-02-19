ae83200cfa068533bd6dda813350ac7ac8b69a55
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-19 10:40:01 -0600

    updated __init__.py

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -3,10 +3,5 @@ from .tag import Tag
 from .comment import Comment
 from .rareuser import RareUser
 from .category import Category
-<<<<<<< HEAD
-# from .post_tag import Post_tag
-# from .account_type import Account_Type 
-=======
 from .posttag import PostTag
 from .account_type import Account_Type 
->>>>>>> origin/ep-tags-models-fixtures
