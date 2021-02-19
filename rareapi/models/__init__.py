557c6b1bc408fbb238c19c972f50cb5a8cd3d793
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-19 11:18:04 -0600

    init changes

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -2,16 +2,6 @@ from .rareuser import RareUser
 from .post import Post
 from .tag import Tag
 from .comment import Comment
-<<<<<<< HEAD
-from .rareuser import RareUser
 from .category import Category
 from .posttag import PostTag
 
-=======
-from .category import Category
-from .posttag import PostTag
-<<<<<<< HEAD
-from .accounttype import AccountType 
->>>>>>> origin/lr-rareuser-model
-=======
->>>>>>> main
