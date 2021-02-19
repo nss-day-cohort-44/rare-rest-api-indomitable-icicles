cd938a89d6dca9b753000c90723fe36fd78f721a
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-19 15:56:06 -0600

    trying to configure urls.py

--- rare/urls.py
+++ rare/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """rare URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,31 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.urls import path
-from rest_framework import routers
-# from rareapi.views import register_user,login_user
-from rareapi.views import Comments
-
-router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'comments', Comments, 'comment')
-
-urlpatterns = [
-    path('', include(router.urls)),
-    # path('register', register_user),
-    # path('login', login_user),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-]
-=======
 from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
 from rareapi.views import register_user, login_user
-
-
+from rareapi.views import Comments
 
 router = routers.DefaultRouter(trailing_slash=False)
-
+router.register(r'comments', Comments, 'comment')
 
 
 urlpatterns = [
@@ -47,4 +29,3 @@ urlpatterns = [
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
->>>>>>> origin/lr-server-register
