61fc71bbb6d5420ab0cbe6ff7f1794ba69131025
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-19 10:05:47 -0600

    workin on reformatting Comment model to fit Django patterns

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -1,9 +1,12 @@
+from django.db import models
+
+from .game import Game
+
 class Comment():
-    def __init__(self, id, post_id, author_id, content, created_on, username):
-        self.id = id
-        self.post_id = post_id
-        self.author_id = author_id
-        self.content = content
-        self.created_on = created_on
-        self.username = username
+        
+        post = post
+        author_id = author_id
+        content = content
+        created_on = created_on
+        username = username
         
