6b32718605c5b1170283703a476eae1ce6a4d57e
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-24 10:35:02 -0600

    changes made to views/post.py

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -5,12 +5,8 @@ from rest_framework import status
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-<<<<<<< HEAD
 from django.contrib.auth.models import User
-from rareapi.models import Post, Category, RareUser, rareuser
-=======
 from rareapi.models import Post, Category, RareUser, rareuser, PostTag, Tag
->>>>>>> main
 
 
 class Posts(ViewSet):
